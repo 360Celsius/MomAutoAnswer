a9345d4d8f6bc1ab50a65b748236b6a3ef95403a
Dennis Shar <dennis.shar@servicenow.com>
2019-05-04 21:34:19 +0300

    send auto message added

--- Mom/app/src/main/java/com/celsius/mom/services/IncomingPushNotificationService.java
+++ Mom/app/src/main/java/com/celsius/mom/services/IncomingPushNotificationService.java
@@ -1,9 +1,13 @@
 package com.celsius.mom.services;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.widget.Toast;
 
 public class IncomingPushNotificationService extends NotificationListenerService {
 
@@ -29,17 +33,33 @@ public class IncomingPushNotificationService extends NotificationListenerService
             String postTime = String.valueOf(sbn.getPostTime());
 
 
+            if(title.equalsIgnoreCase("Dennis Shar") && text.equalsIgnoreCase("Hi"))
+                sendSMS("972543129184","Go Away");
 
-        }catch (NullPointerException e){
+
+
+        }catch (Exception e){
             e.printStackTrace();
         }
 
     }
 
     @Override
-
     public void onNotificationRemoved(StatusBarNotification sbn) {
         Log.i("Msg","Notification Removed");
 
+    }
+
+    public void sendSMS(String phoneNo, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
     }
 }
